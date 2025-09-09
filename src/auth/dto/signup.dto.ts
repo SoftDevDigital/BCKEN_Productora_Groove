f5358569b3e314e4eb812670f9c057454b520985
AlexAc0226 <alexis.correa026@gmail.com>
2025-09-09 13:48:27 -0300

    Renombrar campos de nombre y apellido a name y last_name en DTO y servicios de autenticación.

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -3,11 +3,11 @@ import { IsEmail, IsString, MinLength, IsNotEmpty } from 'class-validator';
 export class SignUpDto {
   @IsNotEmpty()
   @IsString()
-  nombre: string; // given_name
+  name: string; // given_name
 
   @IsNotEmpty()
   @IsString()
-  apellido: string; // family_name
+  last_name: string; // family_name
 
   @IsEmail()
   email: string;
