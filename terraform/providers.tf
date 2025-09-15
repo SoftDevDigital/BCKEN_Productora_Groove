6c4dff4e115ba6407250a57697753152efb3cd85
AlexAc0226 <alexis.correa026@gmail.com>
2025-09-15 19:45:25 -0300

    feat: agregar configuración de S3 y DynamoDB para gestión de tickets y eventos

---
+++ terraform/providers.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAZUZPKJAY4BFMJIVI"  # Reemplaza con tus credenciales reales o usa AWS CLI
+  secret_key = "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"  # Reemplaza con tus credenciales reales o usa AWS CLI
+}
