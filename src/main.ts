7f8e3313ca55938921dd2ca512776aa8940cd2e8
AlexAc0226 <alexis.correa026@gmail.com>
2025-09-16 18:37:41 -0300

    fix: simplificar configuración de CORS y eliminar lógica innecesaria en bootstrap

--- src/main.ts
+++ src/main.ts
@@ -7,27 +7,12 @@ let server: Handler;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // Lista de orígenes permitidos para CORS
-  const allowedOrigins = [
-    'http://localhost:3000', // Para desarrollo local
-    'https://fest-go.com', // Dominio principal en producción
-  ];
-
   app.enableCors({
-    origin: (origin, callback) => {
-      // Permitir solicitudes sin origen (como las de herramientas como Postman)
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('No permitido por CORS'));
-      }
-    },
+    origin: 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
   });
-
   await app.init();
   const expressApp = app.getHttpAdapter().getInstance();
   return serverlessExpress({ app: expressApp });
@@ -47,15 +32,12 @@ export const handler: Handler = async (
 if (process.env.NODE_ENV !== 'production') {
   async function startLocal() {
     const app = await NestFactory.create(AppModule);
-
-    // Configuración de CORS para desarrollo local
     app.enableCors({
-      origin: 'http://localhost:3000',
+      origin: 'https://fest-go.com',
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
       allowedHeaders: 'Content-Type, Authorization',
       credentials: true,
     });
-
     await app.listen(process.env.PORT || 3001);
   }
   startLocal();
