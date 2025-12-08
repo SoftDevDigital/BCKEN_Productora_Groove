2de078077a1202a113249380ee29cd30875f9532
AlexAc0226 <alexis.correa026@gmail.com>
2025-12-08 15:35:13 -0300

    fix(cors): restrict CORS origin to specific domains for enhanced security

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: ['http://localhost:3000', 'https://fest-go.com'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: [
       'Content-Type',
